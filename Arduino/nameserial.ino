8b7fef44394b48f77579981aa90214bc14289a52
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2022-09-15 19:30:24 +0200

    Create nameserial.ino

---
+++ Arduino/nameserial.ino
@@ -0,0 +1,10 @@
+
+void setup() {   //setup 1x after restart
+Serial.begin(9600); // start serial communication at 9600 bits per second
+}
+
+
+void loop() {  // loop
+Serial.println("victqr"); // print my name out
+delay(1000); // delay of 1 second
+}
