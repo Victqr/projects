8078a2105abaa0e8405f0182f726069363fd7271
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2022-09-29 21:08:40 +0200

    Update counting.ino

--- Arduino/Serial/counting.ino
+++ Arduino/Serial/counting.ino
@@ -1,12 +1,14 @@
+int coutingnumber = 25;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("start counting to 25"); // Send start counting
+  Serial.println(String("starting counting to ") + coutingnumber); // Send start counting
 }
 void loop() {
-  for (int Counting=1; Counting<=25; ++Counting) {
+  for (int Counting=1; Counting<=coutingnumber; ++Counting) {
     Serial.println(Counting); // Send counting number
     delay(1000);
   }
-  Serial.println("Counting to 25 done"); // Send stop counting
+  Serial.println(String("Done counting to ") + coutingnumber); // Send stop counting
   while(1);
 }
