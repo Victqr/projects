0e5c0168ac13169d9c515d7ee3536e9cb9a572d7
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2022-09-29 21:04:51 +0200

    Update 2leds_system_code.ino

--- Arduino/Led/2leds_system_code.ino
+++ Arduino/Led/2leds_system_code.ino
@@ -1,25 +1,30 @@
-const int ledPin = 10; //led connectie on pin 10
-const int ledPin2 = 11; //led connectie on pin 11
-const int onLedTime = 200; //delay time 200 ms
-const int offLedTime = 100; //delay time 100 ms
+// C++ code
+int ledPin1 = 10; //led connectie on pin 10
+int ledPin2 = 11;
+int onLedTime = 200; //delay time 500 ms
+int offLedTime = 100; //delay time 500 ms
+int repeat1 = 10; // let loop 1 repeat 10 times
+int repeat2 = 15; // let loop 2 repeat 5 times (you need to calculate how much you wil repaet after loop 1 that 10 times repeat)
+
 
 void setup() {
-  pinMode (ledPin, OUTPUT); //Set LedPin if Output, LedPin is identify on pin 10
-    pinMode (ledPin2, OUTPUT); //Set LedPin2 if Output, LedPin is identify on pin 11
+  pinMode (ledPin1, OUTPUT); //Set LedPin1 if Output, LedPin1 is identify on pin 10
+	pinMode (ledPin2, OUTPUT); //Set LedPin2 if Output, LedPin2 is identify on pin 11
+  
 }
 
 void loop(){
-  for (int i=0; i<10; i++){
-    digitalWrite(ledPin, HIGH);  // turn LEDPin on
-    delay(onLedTime);              // pause
-    digitalWrite(ledPin, LOW);  // turn LEDPin off
-    delay(offLedTime);             // pause
+  for (int i=0; i<repeat1; i++){
+    digitalWrite(ledPin1, HIGH);  //Turn led 1 on
+    delay(onLedTime);              //pause
+    digitalWrite(ledPin1, LOW);  //Turn led 1 off
+    delay(offLedTime);             //pause
   }
-  for (int i=10; i<15; i++){
-    digitalWrite(ledPin2, HIGH);  // turn LEDPin2 onn
-    delay(onLedTime);              // pause
-    digitalWrite(ledPin2, LOW);  // turn LEDPin2 off
-    delay(offLedTime);             // pause
+    for (int i=repeat1; i<repeat2; i++){
+    digitalWrite(ledPin2, HIGH);  //Turn led 2 on
+    delay(onLedTime);              //pause
+    digitalWrite(ledPin2, LOW);  //Turn led 2 off
+    delay(offLedTime);             //pause
   }
-  while(1); //stop whole loop
+  while(1);
 }
