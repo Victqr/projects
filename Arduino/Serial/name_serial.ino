1ef7bf4dc24f17b13fbb49ca868194cea842fd15
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2022-09-29 21:14:04 +0200

    Update name_serial.ino

--- Arduino/Serial/name_serial.ino
+++ Arduino/Serial/name_serial.ino
@@ -1,10 +1,8 @@
-
 void setup() {   //setup 1x after restart
 Serial.begin(9600); // start serial communication at 9600 bits per second
 }
 
-
 void loop() {  // loop
-Serial.println("victqr"); // print my name out
+Serial.println("victar"); // print my name out
 delay(1000); // delay of 1 second
 }
