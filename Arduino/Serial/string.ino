05af0fb29989d227236bd260c2adaf4239e8d586
Victor Limpens <68400813+Victqr@users.noreply.github.com>
2022-10-21 08:44:05 +0200

    Update string.ino

--- Arduino/Serial/string.ino
+++ Arduino/Serial/string.ino
@@ -4,13 +4,14 @@ void setup() {
 	Serial.begin(9600);
 	Serial.println("Typ een tekst in de serial monitor");
 
-
-}
-  
-void loop() {
 	while (Serial.available()==1){
        	message=Serial.readString();
         lengte=message.length(); 
    		Serial.println(lengte);
 	}
+
+}
+  
+void loop() {
+
 }
